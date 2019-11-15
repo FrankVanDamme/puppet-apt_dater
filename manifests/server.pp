b30efa396833036e0c16454bbf3ba8070ce241fe
Frank Van Damme <frank.vandamme@gmail.com>
2019-11-15 15:21:54 +0100

    convert hosts.conf file to xml on change; make sure it  is used by changing the hosts file to it in the system wide configuration

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,14 @@
 class apt_dater::server {
+    $cnf_sys =  "/etc/apt-dater/apt-dater.xml"
+    $hosts_conf_root = "/root/.config/apt-dater/hosts.conf"
+    $hosts_xml_root = "/root/.config/apt-dater/xml.conf"
+
     package { "apt-dater": 
 	ensure => present,
+    }
+
+    -> file { $cnf_sys:
+        content => template("$module_name/apt-dater.xml.system.erb"),
     }
 
     Apt_dater::Section <<| |>>
@@ -9,5 +17,9 @@ class apt_dater::server {
 	ensure => directory,
     }
 
-    concat { "/root/.config/apt-dater/hosts.conf": }
+    concat { $hosts_conf_root: }
+
+    -> exec { "/usr/lib/x86_64-linux-gnu/apt-dater/hosts2xml -i $hosts_conf_root -o $hosts_xml_root":
+        refreshonly =>  true,
+    }
 }
