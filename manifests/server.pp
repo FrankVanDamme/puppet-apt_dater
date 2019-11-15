c5abd6f6d20a522922a0b0e92d74ef4d226f9e48
Frank Van Damme <frank.vandamme@gmail.com>
2019-11-15 15:39:28 +0100

    fix in refreshing the hosts.xml file

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,13 +1,11 @@
 class apt_dater::server {
     $cnf_sys =  "/etc/apt-dater/apt-dater.xml"
     $hosts_conf_root = "/root/.config/apt-dater/hosts.conf"
-    $hosts_xml_root = "/root/.config/apt-dater/xml.conf"
+    $hosts_xml_root = "/root/.config/apt-dater/hosts.xml"
 
     package { "apt-dater": 
 	ensure => present,
-    }
-
-    -> file { $cnf_sys:
+    } -> file { $cnf_sys:
         content => template("$module_name/apt-dater.xml.system.erb"),
     }
 
@@ -17,9 +15,9 @@ class apt_dater::server {
 	ensure => directory,
     }
 
-    concat { $hosts_conf_root: }
+    concat { $hosts_conf_root: } 
 
-    -> exec { "/usr/lib/x86_64-linux-gnu/apt-dater/hosts2xml -i $hosts_conf_root -o $hosts_xml_root":
+    ~> exec { "/usr/lib/x86_64-linux-gnu/apt-dater/hosts2xml -i $hosts_conf_root -o $hosts_xml_root":
         refreshonly =>  true,
     }
 }
